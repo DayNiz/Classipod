35e217a532b49f90f260d9c584e5da074d031729
Aditya <adeeteya@gmail.com>
2025-10-25 20:26:17 -0500

    🐛 fix online thumbnail path

--- lib/core/constants/online_audio_files_metadata.dart
+++ lib/core/constants/online_audio_files_metadata.dart
@@ -17,7 +17,7 @@ final List<MusicMetadata> onlineDemoAudioFilesMetaData = [
     filePath:
         "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/mp3/Faded.mp3",
     thumbnailPath:
-        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/FadedbyAlanWalker.jpg",
+        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/thumbnails/FadedbyAlanWalker.jpg",
   ),
   MusicMetadata(
     trackName: "Spectre",
@@ -37,7 +37,7 @@ final List<MusicMetadata> onlineDemoAudioFilesMetaData = [
     filePath:
         "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/opus/Spectre.opus",
     thumbnailPath:
-        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/SpectrebyAlanWalker.jpg",
+        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/thumbnails/SpectrebyAlanWalker.jpg",
   ),
   MusicMetadata(
     trackName: "Firefly",
@@ -56,7 +56,7 @@ final List<MusicMetadata> onlineDemoAudioFilesMetaData = [
     filePath:
         "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ogg/Firefly.ogg",
     thumbnailPath:
-        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/FireflybyJimYosef.jpg",
+        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/thumbnails/FireflybyJimYosef.jpg",
   ),
   MusicMetadata(
     trackName: "On & On",
@@ -75,6 +75,6 @@ final List<MusicMetadata> onlineDemoAudioFilesMetaData = [
     filePath:
         "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/m4a/On&On.m4a",
     thumbnailPath:
-        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/On&OnbyCartoon&DanielLevi.jpg",
+        "https://raw.githubusercontent.com/adeeteya/Classipod/master/test/test_files/ClassiPod/thumbnails/On&OnbyCartoon&DanielLevi.jpg",
   ),
 ];
